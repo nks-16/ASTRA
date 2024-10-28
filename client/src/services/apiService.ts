415496078fcb67eb438eed812142f623a2509fd9
Vamsi Nune <99815466+vamsireddynune@users.noreply.github.com>
2024-10-28 09:50:15 +0530

    Update apiService.ts

--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const fetchSampleData = () => {
-  return axios.get<{ id: number; name: string }[]>('http://localhost:5010/api/sample');
+  return axios.get<{ id: number; name: string }[]>('http://localhost:5000/api/sample');
 };
