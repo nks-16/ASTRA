5ea3c2cece197b2f52a27e1e5ec82064a7e90bb1
nks-16 <2022cs_karthikeyas_b@nie.ac.in>
2024-12-10 21:24:30 +0530

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://nisb-hackathon.vercel.app', // Allow only this origin
+  origin: 'https://nisb-hackathon.vercel.app/', // Allow only this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
   credentials: true // Allow cookies and credentials if needed
 }));
