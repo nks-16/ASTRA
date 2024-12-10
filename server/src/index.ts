da950cba9ed03e580a072ac9b63f0f0da90527d1
nks-16 <2022cs_karthikeyas_b@nie.ac.in>
2024-12-10 21:17:52 +0530

    Integrated

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,11 @@ const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://nisb-hackathon.vercel.app', // Allow only this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: true // Allow cookies and credentials if needed
+}));
 app.use(express.json());
 
 // Database Connection
